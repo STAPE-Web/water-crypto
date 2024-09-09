34fbd066c6a72f2b9dc2e32ac07d7786770736b3
STAPE <ggggfetfdfgjjh2@gmail.com>
2024-09-09 12:01:56 +0300

    update

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosError, AxiosResponse } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
-    baseURL: "/api",
+    baseURL: import.meta.env.VITE_SERVER,
     headers: {
         'Content-Type': 'application/json',
     },
